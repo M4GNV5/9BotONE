f0d5e8105b4cd284899078b27bef0c2e2916b7cd
Jakob Löw <jakob@m4gnus.de>
2019-05-10 23:42:04 +0200

    add the ability to write to values/registers
    the script nwo has three subcommands:
    - read - reads bytes from the registers
    - write - writes bytes to the registers
    - dump - dumps all registers to a file

--- 9BotONE.py
+++ 9BotONE.py
@@ -1,12 +1,15 @@
 from struct import pack, unpack
-from binascii import hexlify
-import time, queue, pygatt
+from binascii import hexlify, unhexlify
+import sys, queue, pygatt
 
 mac = "<your mac address here>"
 
 # rx/tx for Ninebot, means we write to rx and read from tx
 rx_char_uuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 tx_char_uuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+
+command_read = 0x01
+command_write = 0x02
 
 def receive_callback(handle, value):
 	print("< [%s] %s" % (handle, hexlify(value).upper()))
@@ -26,11 +29,12 @@ def calculate_checksum(data):
 		s += c
 	return (s & 0xFFFF) ^ 0xFFFF
 
-def send_packet(command, data):
+def send_packet(command, offset, data):
 	# see http://www.gorina.es/9BMetrics/protocol.html
-	# 55 AA <len> 09 01 <cmd> [data...] <chk1> <chk2>
+	# 55 AA <len> 11 <cmd> <offset> [data...] <chk1> <chk2> for ONE A1/S2
+	# 55 AA <len> 09 <cmd> <offset> [data...] <chk1> <chk2> for others (untested)
 
-	payload = pack("BBBB", len(data) + 2, 0x11, 0x01, command) + data
+	payload = pack("BBBB", len(data) + 2, 0x11, command, offset) + data
 	checksum = pack("<H", calculate_checksum(payload))
 
 	packet = bytearray("\x55\xAA" + payload + checksum)
@@ -38,28 +42,39 @@ def send_packet(command, data):
 	print("> %s" % hexlify(packet).upper())
 	device.char_write_handle(write_handle, packet)
 
-def read_registers(offset, length=1):
+def read_packet():
+	packet_header = read_response(6)
+	magic1, curr_len, magic2, offset = unpack("<HBHB", packet_header)
+
+	curr_len -= 2
+	payload = read_response(curr_len)
+	read_response(2) #checksum
+
+	return payload
+
+def read_bytes(offset, length=1):
 	# see http://www.gorina.es/9BMetrics/protocol.html
-	# <offset> <read length>
+	# 0x01 <offset> <read length>
 
 	data = bytearray([length])
-	send_packet(offset, data)
+	send_packet(command_read, offset, data)
 
 	i = 0
 	result = bytearray(length)
 	while i < length:
-		packet_header = read_response(6)
-		magic_header, curr_len, magic_header2, command = unpack("<HBHB", packet_header)
-
-		curr_len -= 2
-		payload = read_response(curr_len)
-		read_response(2) #checksum
-
-		for j in range(0, curr_len):
-			result[i] = payload[j]
+		payload = read_packet()
+		for c in payload:
+			result[i] = c
 			i += 1
 
 	return result
+
+def write_bytes(offset, data):
+	# see http://www.gorina.es/9BMetrics/protocol.html
+	# 0x02 <offset> <bytes ...>
+
+	send_packet(command_write, offset, data)
+	return read_packet()
 
 receive_queue = queue.Queue()
 
@@ -71,9 +86,36 @@ device.subscribe(tx_char_uuid, callback=receive_callback)
 
 write_handle = device.get_handle(rx_char_uuid)
 
-with open("registers.bin", "w+") as fd:
-	data = read_registers(0, 0x80)
-	fd.write(data)
+if sys.argv[1] == "read":
+	pos = int(sys.argv[2], 0)
+	length = int(sys.argv[3], 0)
 
-	data = read_registers(0x80, 0x80)
-	fd.write(data)
+	print("reading %d bytes at %d" % (length, pos))
+	data = read_bytes(pos, length)
+
+	print(hexlify(data).upper())
+
+elif sys.argv[1] == "write":
+	pos = int(sys.argv[2], 0)
+	data = unhexlify(sys.argv[3])
+	data = bytearray(data)
+
+	print("writing %d bytes at %d" % (len(data), pos))
+	data = write_bytes(pos, data)
+
+	print(hexlify(data).upper())
+
+elif sys.argv[1] == "dump":
+	filename = sys.argv[2]
+	print("reading all registers and writing to %s" % filename)
+	with open(filename, "w+") as fd:
+		data = read_bytes(0, 0x80)
+		fd.write(data)
+
+		data = read_bytes(0x80, 0x80)
+		fd.write(data)
+
+else:
+	print("python 9BotONE.py read <offset> <length>")
+	print("python 9BotONE.py write <offset> <hex bytes...>")
+	print("python 9BotONE.py dump <file>")
